da36461f6808280b99059c0123a41a7ea81d09a1
Palm <palml1lax@gmail.com>
2023-12-24 22:25:04 +0700

    feat: finish ex 2-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import axios from "axios";
 import "./App.css";
-
+import { useEffect, useState } from "react";
+import { DebounceInput } from "react-debounce-input";
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [books, setBooks] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const getBooks = async () => {
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=${search}`
+        );
+        setBooks([...response.data.items]);
+      } catch (error) {
+        console.log(error);
+      }
+      if (search === "") {
+        setBooks([]);
+      }
+    };
+
+    getBooks();
+  }, [search]);
+
+  console.log(books);
+  return (
+    <div className="App">
+      <DebounceInput
+        type="text"
+        debounceTimeout={200}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <ul>
+        {books.map(({ volumeInfo: { title } }, index) => {
+          return <li key={index}>{title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
